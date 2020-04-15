3115e2704929ec137b521184135c2bb279b7ee91
Martin Ihrke <miadmin@azihrke.onmicrosoft.com>
2020-04-15 15:09:45 +0200

    filter secrets

--- GetAKVSecrets.ps1
+++ GetAKVSecrets.ps1
@@ -6,11 +6,16 @@ $vaultName = $config.vaultName
 $appID = $config.appID
 $secret = $config.secret
 $tenantID = $config.tenantID
+$inXDays = $config.inXDays
+
+$endDate = (Get-Date).AddDays($inXDays)
+
+Write-Host $endDate
 
 #authicate with SPN credentials
 $passwd = ConvertTo-SecureString $secret -AsPlainText -Force
 $pscredential = New-Object System.Management.Automation.PSCredential($appID, $passwd)
 Connect-AzAccount -ServicePrincipal -Credential $pscredential -Tenant $tenantId
 
-#get all secrets
-Get-AzKeyVaultSecret -VaultName $vaultName
+#get all secrets, filter for secrets without expiration date and expiration date nearer than set 
+Get-AzKeyVaultSecret -VaultName $vaultName | Where-Object {$_.Expires -ne $null} | Where-Object {$_.Expires -lt $endDate} 
